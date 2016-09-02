89bd8f44aaadb482bd3c906c9e44e23b62920797
maprohu <maprohu@gmail.com>
2016-09-02 17:28:37 +0100

    vis.js

--- scalajs/analyzer/src/main/scala/toolbox/builder/scalajs/analyzer/processor/OptionsProcessor.scala
+++ scalajs/analyzer/src/main/scala/toolbox/builder/scalajs/analyzer/processor/OptionsProcessor.scala
@@ -7,6 +7,7 @@ import toolbox.builder.scalajs.analyzer.model.QualifiedName
 
 import scala.collection.immutable.Seq
 import scala.reflect.runtime.{universe => ru}
+import scala.scalajs.js
 
 /**
   * Created by pappmar on 02/09/2016.
@@ -48,17 +49,31 @@ object OptionsProcessor {
       val classSymbol = mirror.classSymbol(optionsClass)
       val cs = classSymbol.typeSignature
 
-      cs
-        .decls
-        .filter(_.isTerm)
-        .map(_.asTerm)
-        .filter(t => t.isVal || t.isVar)
-        .map({ t =>
-          (t.name.toString, t.typeSignature.toString)
-        })
-        .foreach({ t =>
-          println(t)
-        })
+      val fields =
+        cs
+          .decls
+          .filter(_.isTerm)
+          .map(_.asTerm)
+          .filter(t => t.isVal || t.isVar)
+          .map({ t =>
+            (t.name.toString, t.typeSignature.toString, t.typeSignature.erasure == ru.typeOf[js.UndefOr[Any]])
+          })
+
+      val args =
+        fields
+          .map({
+            case (name, typ, undef) =>
+              s"    ${name}: ${typ}${if (undef) " = scala.scalajs.js.undefined" else ""}"
+          })
+          .mkString(",\n")
+
+      val params =
+        fields
+          .map({
+            case (name, _, _) =>
+              s"    ${name} = ${name}"
+          })
+          .mkString(",\n")
 
       IO.write(
         targetFile,
@@ -66,7 +81,9 @@ object OptionsProcessor {
            |
            |object ${factoryClassName} {
            |  def apply(
+           |${args}
            |  ) : ${className} = scala.scalajs.js.Dynamic.literal(
+           |${params}
            |  ).asInstanceOf[${className}]
            |}
          """.stripMargin
