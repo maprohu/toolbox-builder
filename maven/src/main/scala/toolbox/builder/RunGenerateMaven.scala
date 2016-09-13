713ec8de66f870ba49adc35a7054dbf1c0bbe7a0
maprohu <maprohu@gmail.com>
2016-09-13 15:04:01 +0100

    efca

--- maven/src/main/scala/toolbox/builder/RunGenerateMaven.scala
+++ maven/src/main/scala/toolbox/builder/RunGenerateMaven.scala
@@ -44,6 +44,7 @@ object RunGenerateMaven {
     "de.heikoseeberger:akka-http-json4s_2.11:jar:1.9.0",
     "org.json4s:json4s-native_2.11:jar:3.4.0",
     "emsa:wupdata-common-shared:jar:1.0.4-SNAPSHOT",
+    "emsa:wupdata-core:jar:1.0.5-SNAPSHOT",
     "com.github.nscala-time:nscala-time_2.11:jar:2.12.0"
   )
 
